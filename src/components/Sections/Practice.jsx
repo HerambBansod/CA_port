484507ce7a76bf28b82c924c95ffb6562d5eec32
HerambBansod <heramb.b744@gmail.com>
2025-01-28 02:37:40 +0530

    Ensure practice.css exists

--- src/components/Sections/Practice.jsx
+++ src/components/Sections/Practice.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './practice.css'
+import '../Sections/Practice.css';
 import { Link } from 'react-router-dom';
 
 import { FaBriefcase, FaRegFileAlt, FaGlobe, FaBuilding, FaCheck } from 'react-icons/fa';
