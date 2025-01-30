34f7c29aca313e964ba3e8cb33df7b8c305b6b97
HerambBansod <heramb.b744@gmail.com>
2025-01-30 11:17:07 +0530

    responsive thing done

--- src/components/Contact US Page/Contact_Us_page.jsx
+++ src/components/Contact US Page/Contact_Us_page.jsx
@@ -17,7 +17,7 @@ export default function Contact_Us_page() {
                     <div className="contact-left">
                         <div className="map">
                             <iframe
-                               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.1234567890123!2d73.91234567890123!3d18.551234567890123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c1234567890%3A0xabcdef1234567890!2sVadgaon%20Sheri%2C%20Pune%2C%20Maharashtra%20411014%2C%20India!5e0!3m2!1sen!2sus!4v1610000000000!5m2!1sen!2sus"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.1234567890123!2d73.91234567890123!3d18.551234567890123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c1234567890%3A0xabcdef1234567890!2sVadgaon%20Sheri%2C%20Pune%2C%20Maharashtra%20411014%2C%20India!5e0!3m2!1sen!2sus!4v1610000000000!5m2!1sen!2sus"
                                 width="100%"
                                 height="250"
                                 style={{ border: 0, borderRadius: '8px' }}
